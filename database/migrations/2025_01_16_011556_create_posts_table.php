570d545d9d9bc1bf10c38580d07840fe333b317d
D-WILD\Wild <wmp84@hotmail.com>
2025-01-15 20:27:16 -0500

    Migraciones campos

--- database/migrations/2025_01_16_011556_create_posts_table.php
+++ database/migrations/2025_01_16_011556_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,13 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->string('title');
+            $table->text('content');
+            $table->string('image_path')->nullable();
+            $table->boolean('is_published')->default(false);
             $table->timestamps();
         });
     }
--- database/migrations/2025_01_16_012358_create_post_tag_table.php
+++ database/migrations/2025_01_16_012358_create_post_tag_table.php
@@ -4,16 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('post_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->foreignId('tag_id')
+                ->constrained()
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -23,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('post_tag');
     }
 };
