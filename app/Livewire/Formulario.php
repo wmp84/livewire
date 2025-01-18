c8d76f89ecc6754fc4eff505482688b61c283889
D-WILD\Wild <wmp84@hotmail.com>
2025-01-18 12:57:21 -0500

    Post Mostrar

--- app/Livewire/Formulario.php
+++ app/Livewire/Formulario.php
@@ -12,25 +12,23 @@ class Formulario extends Component
     public $categories, $tags;
     public $category_id = '', $title, $content;
     public $selectedTags = [];
+    public $posts;
 
     public function mount()
     {
         $this->categories = Category::all();
         $this->tags = Tag::all();
+        $this->posts = Post::all();
     }
 
     public function save()
     {
-        /*$post = Post::create([
-            'category_id' => $this->category_id,
-            'title' => $this->title,
-            'content' => $this->content
-        ]);*/
         $post = Post::create(
             $this->only('category_id', 'title', 'content')
         );
         $post->tags()->attach($this->selectedTags);
         $this->reset(['category_id', 'title', 'content', 'selectedTags']);
+        $this->posts = Post::all();
     }
 
     public function render()
--- resources/views/livewire/formulario.blade.php
+++ resources/views/livewire/formulario.blade.php
@@ -1,39 +1,51 @@
-<div class="bg-white shadow rounded-lg p-6">
-    <form wire:submit="save">
-        <div class="mb-4">
-            <x-label>Nombre</x-label>
-            <x-input class="w-full" wire:model="title" required/>
-        </div>
-        <div class="mb-4">
-            <x-label>Contenido</x-label>
-            <x-textarea class="w-full" wire:model="content" required/>
-        </div>
-        <div class="mb-4">
-            <x-label>Categoría</x-label>
-            <x-select wire:model="category_id">
-                <option value="" disabled>Seleccione una categoría</option>
-                @foreach($categories as $category)
-                    <option value="{{$category->id}}">
-                        {{$category->name}}
-                    </option>
-                @endforeach
-            </x-select>
-        </div>
-        <div class="mb-4">
-            <x-label>Etiquetas</x-label>
-            <ul>
-                @foreach($tags as $tag)
-                    <li>
-                        <x-label>
-                            <x-checkbox wire:model="selectedTags" value="{{$tag->id}}"/>
-                            {{$tag->name}}
-                        </x-label>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-        <div class="flex justify-end">
-            <x-button>Crear</x-button>
-        </div>
-    </form>
+<div>
+    <div class="bg-white shadow rounded-lg p-6 mb-8">
+        <form wire:submit="save">
+            <div class="mb-4">
+                <x-label>Nombre</x-label>
+                <x-input class="w-full" wire:model="title" required/>
+            </div>
+            <div class="mb-4">
+                <x-label>Contenido</x-label>
+                <x-textarea class="w-full" wire:model="content" required/>
+            </div>
+            <div class="mb-4">
+                <x-label>Categoría</x-label>
+                <x-select wire:model="category_id">
+                    <option value="" disabled>Seleccione una categoría</option>
+                    @foreach($categories as $category)
+                        <option value="{{$category->id}}">
+                            {{$category->name}}
+                        </option>
+                    @endforeach
+                </x-select>
+            </div>
+            <div class="mb-4">
+                <x-label>Etiquetas</x-label>
+                <ul>
+                    @foreach($tags as $tag)
+                        <li>
+                            <x-label>
+                                <x-checkbox wire:model="selectedTags" value="{{$tag->id}}"/>
+                                {{$tag->name}}
+                            </x-label>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="flex justify-end">
+                <x-button>Crear</x-button>
+            </div>
+        </form>
+    </div>
+    <div class="bg-white shadow rounded-lg p-6">
+        <ul class="list-disc list-inside">
+        @forelse($posts as $post)
+            <li>
+                {{$post->title}}
+            </li>
+            @empty
+        @endforelse
+        </ul>
+    </div>
 </div>
